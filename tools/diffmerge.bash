22d3ba36b0907931cd01d8b883a0fcae4097d3f9
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2013-08-08 08:38:00 +0200

    merge tool

---
+++ tools/diffmerge.bash
@@ -0,0 +1,3 @@
+function clj-diffmerge {
+  diffmerge /home/nanchen/engineering/java/clojure/lang/$1 /home/nanchen/engineering/clojure/clojure/src/jvm/clojure/lang/$1 /home/nanchen/engineering/java/clj-ds/src/main/java/com/trifork/clj_ds/$1
+}
